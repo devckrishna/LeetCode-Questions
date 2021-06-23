dfb633df651a7a849f3b71b33268cb586ef703be
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2021-06-23 14:51:25 +0530

    some

---
+++ Heaps/KClosestPointsToOrigin.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class KClosestPointsToOrigin {
+  public int[][] kClosest(int[][] points, int K) {
+
+    PriorityQueue<int[]> heap = new PriorityQueue<int[]>(new Comparator<int[]>() {
+      @Override
+      public int compare(int[] left, int[] right) {
+        return getDistance(right) - getDistance(left);
+      }
+    });
+
+    for (int[] point : points) {
+      heap.add(point);
+      if (heap.size() > K)
+        heap.poll();
+    }
+
+    int[][] result = new int[K][2];
+    while (K > 0)
+      result[--K] = heap.poll();
+
+    return result;
+
+  }
+
+  private int getDistance(int[] point) {
+    return point[0] * point[0] + point[1] * point[1];
+  }
+}

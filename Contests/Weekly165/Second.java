e1e3aa7dbf442e9259cfbf60a1242295dcfec92b
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2021-11-11 12:22:59 +0530

    Some

---
+++ Contests/Weekly165/Second.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Second {
+  public List<Integer> numOfBurgers(int t, int c) {
+    return t % 2 == 0 && c * 2 <= t && t <= c * 4 ? Arrays.asList(t / 2 - c, c * 2 - t / 2) : new ArrayList<>();
+  }
+}
---
+++ Contests/Weekly165/Third.java
@@ -0,0 +1,20 @@
+public class Third {
+  public int countSquares(int[][] matrix) {
+    int[][] dp = new int[matrix.length][matrix[0].length];
+
+    int ans = 0;
+    for (int i = 0; i < dp.length; i++) {
+      for (int j = 0; j < dp[0].length; j++) {
+        if (i == 0 || j == 0) {
+          dp[i][j] = matrix[i][j];
+        } else {
+          if (matrix[i][j] == 1)
+            dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j]));
+        }
+        ans += dp[i][j];
+      }
+
+    }
+    return ans;
+  }
+}

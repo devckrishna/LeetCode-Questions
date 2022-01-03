914a880aa0353deacb991a36f1a1308d1887f03e
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2022-01-03 10:11:13 +0530

    some

--- JanChallenge/FindTheTownJudge.java
+++ JanChallenge/FindTheTownJudge.java
@@ -1,12 +1,12 @@
 public class FindTheTownJudge {
-  public int findJudge(int N, int[][] trust) {
-    int[] count = new int[N + 1];
-    for (int[] t : trust) {
-      count[t[0]]--;
-      count[t[1]]++;
+  public int findJudge(int n, int[][] trust) {
+    int[] arr = new int[n + 1];
+    for (int[] curr : trust) {
+      arr[curr[0]]--;
+      arr[curr[1]]++;
     }
-    for (int i = 1; i <= N; ++i) {
-      if (count[i] == N - 1)
+    for (int i = 1; i <= n; ++i) {
+      if (arr[i] == n - 1)
         return i;
     }
     return -1;

8422269ac6058533d21d20d6b27681f440536c42
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2022-01-03 13:20:20 +0530

    Sime

--- JanChallenge/FindTheTownJudge.java
+++ JanChallenge/FindTheTownJudge.java
@@ -1,13 +1,19 @@
+import java.util.*;
+
 public class FindTheTownJudge {
   public int findJudge(int n, int[][] trust) {
-    int[] arr = new int[n + 1];
-    for (int[] curr : trust) {
-      arr[curr[0]]--;
-      arr[curr[1]]++;
+    if (trust.length == 0 && n == 1) {
+      return 1;
     }
-    for (int i = 1; i <= n; ++i) {
-      if (arr[i] == n - 1)
-        return i;
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int[] curr : trust) {
+      map.put(curr[0], map.getOrDefault(curr[0], 0) - 1);
+      map.put(curr[1], map.getOrDefault(curr[1], 0) + 1);
+    }
+    for (int temp : map.keySet()) {
+      if (map.get(temp) == n - 1) {
+        return temp;
+      }
     }
     return -1;
   }

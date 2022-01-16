af642bb273826103bcff8edf146e40c2403c3961
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2022-01-16 09:59:16 +0530

    some

---
+++ JanChallenge/MaxDistanceToTheClosestPerson.java
@@ -0,0 +1,23 @@
+public class MaxDistanceToTheClosestPerson {
+  public int maxDistToClosest(int[] seats) {
+    int ans = 0;
+    int left = -1;
+    for (int i = 0; i < seats.length; i++) {
+      if (seats[i] == 0) {
+        continue;
+      }
+      if (left == -1) {
+        ans = i;
+      } else {
+        ans = Math.max(ans, (i - left) / 2);
+      }
+      left = i;
+    }
+
+    if (seats[seats.length - 1] == 0) {
+      ans = Math.max(ans, seats.length - left - 1);
+    }
+
+    return ans;
+  }
+}

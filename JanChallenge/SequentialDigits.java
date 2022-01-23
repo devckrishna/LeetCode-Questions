36acbdb26ba660765be7e6faff2bff082770ac41
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2022-01-23 11:12:36 +0530

    Some

--- JanChallenge/SequentialDigits.java
+++ JanChallenge/SequentialDigits.java
@@ -3,16 +3,23 @@ import java.util.*;
 public class SequentialDigits {
   public List<Integer> sequentialDigits(int low, int high) {
     List<Integer> res = new ArrayList<>();
-    for (int digit = 1; digit < 9; ++digit) {
-      int next = digit, n = next;
-      while (n <= high && next < 10) {
-        if (n >= low) {
-          res.add(n);
-        }
-        n = n * 10 + ++next;
-      }
+    for (int i = 1; i <= 9; i++) {
+      solve(0, i, low, high, res);
     }
     Collections.sort(res);
     return res;
+  }
+
+  public void solve(int cur, int nums, int low, int high, List<Integer> res) {
+    cur = cur * 10 + nums;
+    if (cur > high)
+      return;
+
+    if (cur >= low) {
+      res.add(cur);
+    }
+
+    if (nums + 1 <= 9)
+      solve(cur, nums + 1, low, high, res);
   }
 }

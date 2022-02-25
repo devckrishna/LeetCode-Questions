981507fb218ab73089183275a6a210d77d5277aa
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2022-02-25 08:54:42 +0530

    some

---
+++ FebChallenge/CompareVersionNumbers.java
@@ -0,0 +1,20 @@
+public class CompareVersionNumbers {
+  public int compareVersion(String v1, String v2) {
+    for (int i = 0, j = 0; i < v1.length() || j < v2.length(); i++, j++) {
+      int num1 = 0, num2 = 0;
+      while (i < v1.length() && v1.charAt(i) != '.') {
+        num1 = (num1 * 10) + v1.charAt(i) - '0';
+        i++;
+      }
+      while (j < v2.length() && v2.charAt(j) != '.') {
+        num2 = (num2 * 10) + v2.charAt(j) - '0';
+        j++;
+      }
+      if (num1 < num2)
+        return -1;
+      else if (num1 > num2)
+        return 1;
+    }
+    return 0;
+  }
+}

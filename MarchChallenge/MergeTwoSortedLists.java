adb81d608050bd6dc350af91063435c331530165
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2022-03-09 10:02:41 +0530

    some

--- MarchChallenge/MergeTwoSortedLists.java
+++ MarchChallenge/MergeTwoSortedLists.java
@@ -1,4 +1,21 @@
 public class MergeTwoSortedLists {
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
   public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
     if (l1 == null)
       return l2;
